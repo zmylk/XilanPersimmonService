de661043e7ff9d7ced81c5403da1459c104951e2
LiKe <lk11121314151617@163.com>
2020-03-24 23:49:08 +0800

    :sparkles: 添加新特性

--- xilan-baseService/src/test/java/com/xilan/base/ServiceTest.java
+++ xilan-baseService/src/test/java/com/xilan/base/ServiceTest.java
@@ -3,6 +3,7 @@ package com.xilan.base;
 import com.xilan.bengin.BaseServiceApplication;
 import com.xilan.bengin.entity.WXAccessTokenModel;
 import com.xilan.bengin.service.Impl.LoginServiceImpl;
+import com.xilan.bengin.service.TimeOpeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,8 @@ public class ServiceTest {
 
     @Autowired
     private LoginServiceImpl loginService;
+    @Autowired
+    private TimeOpeService timeOpeService;
 
     @Test
     public void getAccessTokenTest(){
@@ -32,5 +35,10 @@ public class ServiceTest {
     @Test
     public void getUserInfoTest(){
         loginService.getUserInfo("o4dta5Ls4UVCXBR3aOImFGtNXbyQ");
+    }
+
+    @Test
+    public void addTime(){
+        timeOpeService.addRecording("o4dta5Ls4UVCXBR3aOImFGtNXbyQ");
     }
 }
