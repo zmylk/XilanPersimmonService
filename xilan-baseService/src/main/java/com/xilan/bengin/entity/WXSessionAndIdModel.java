4aa19b87235cf6f11b3a04f43af1610591ce8c58
LiKe <lk11121314151617@163.com>
2020-03-24 08:21:06 +0800

    微信登陆权限

---
+++ xilan-baseService/src/main/java/com/xilan/bengin/entity/WXSessionAndIdModel.java
@@ -0,0 +1,13 @@
+package com.xilan.bengin.entity;
+
+import lombok.Data;
+
+/**
+ * @author:liwenkang
+ * @date:2019/8/9 14:30
+ */
+@Data
+public class WXSessionAndIdModel {
+    private String session_key;
+    private String openid;
+}
