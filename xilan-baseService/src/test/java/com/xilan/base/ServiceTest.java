750bb287c038273ea907c32cb965fef65e696859
LiKe <lk11121314151617@163.com>
2020-03-24 08:51:17 +0800

    HTTP请求工具类

---
+++ xilan-baseService/src/test/java/com/xilan/base/ServiceTest.java
@@ -0,0 +1,36 @@
+package com.xilan.base;
+
+import com.xilan.bengin.BaseServiceApplication;
+import com.xilan.bengin.entity.WXAccessTokenModel;
+import com.xilan.bengin.service.Impl.LoginServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author like
+ * @version 1.0
+ * @date 2020/3/23 23:15
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BaseServiceApplication.class)
+public class ServiceTest {
+
+    @Autowired
+    private LoginServiceImpl loginService;
+
+    @Test
+    public void getAccessTokenTest(){
+        WXAccessTokenModel accessToken = loginService.getAccessToken();
+        System.out.println(accessToken);
+    }
+
+    //o4dta5Ls4UVCXBR3aOImFGtNXbyQ
+    //o4dta5Ls4UVCXBR3aOImFGtNXbyQ
+    @Test
+    public void getUserInfoTest(){
+        loginService.getUserInfo("o4dta5Ls4UVCXBR3aOImFGtNXbyQ");
+    }
+}
